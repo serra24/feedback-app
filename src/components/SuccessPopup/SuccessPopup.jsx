7819660e730720dfa16d02fcb115468e90f6cef2
Israa Lotfy <152162832+serra24@users.noreply.github.com>
2025-04-27 12:03:43 +0300

    sucess popup done

--- src/components/SuccessPopup/SuccessPopup.jsx
+++ src/components/SuccessPopup/SuccessPopup.jsx
@@ -1,46 +1,84 @@
-import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
-import { motion } from 'framer-motion';
+import React, { useContext } from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { motion } from "framer-motion";
+import sucess from "../../assets/icons/success-icon.svg";
+import CloseIcon from "@mui/icons-material/Close"; // Material-UI Close Icon
+import { LanguageContext } from "../../context/LanguageContext";
 
 // Success Popup Component
 const SuccessPopup = ({ open, message, onClose }) => {
+    const { translations: t } = useContext(LanguageContext);
   if (!open) return null;
 
   return (
     <Box
       sx={{
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        background: '#4CAF50', // Green for success
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        background: "linear-gradient(180deg, #00395D 0%, #13537C 100%)",
         padding: 3,
-        borderRadius: '8px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        borderRadius: "8px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         zIndex: 9999,
-        minWidth: '300px',
-        textAlign: 'center',
-        color: '#fff',
+        minWidth: "300px",
+        height: "auto",
+        textAlign: "center",
+        color: "#fff",
       }}
     >
-      {/* Success Icon */}
-      <Typography variant="h4">✔️</Typography>
+      {/* Close Icon */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          cursor: "pointer",
+        }}
+        onClick={onClose}
+      >
+        <CloseIcon sx={{ color: "#fff", fontSize: "24px" }} />
+      </Box>
 
+      {/* Success Icon */}
+      <img
+        src={sucess}
+        alt="Error Icon"
+        style={{ width: "50px", height: "50px", marginBottom: "20px" }}
+      />
+
+      {/* Success Title */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4 }}
+      >
+        <Typography sx={{ fontWeight: 700, fontSize: "20px" }}>
+          {t.thanks}
+        </Typography>
+      </motion.div>
       {/* Success Message */}
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.4 }}
       >
-        <Typography sx={{ fontFamily: 'Almarai, sans-serif', fontWeight: 500, fontSize: '18px' }}>
+        <Typography
+          sx={{
+            fontFamily: "Almarai, sans-serif",
+            fontWeight: 500,
+            fontSize: "18px",
+          }}
+        >
           {message}
         </Typography>
       </motion.div>
 
-      {/* Close Button */}
-      <motion.div
+     
+      {/* <motion.div
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         transition={{ duration: 0.2 }}
@@ -49,23 +87,23 @@ const SuccessPopup = ({ open, message, onClose }) => {
           variant="contained"
           sx={{
             mt: 2,
-            width: '100%',
+            width: "100%",
             height: 48,
-            borderRadius: '5px',
-            backgroundColor: '#fff',
-            color: '#00395D',
-            fontFamily: 'Almarai, sans-serif',
+            borderRadius: "5px",
+            backgroundColor: "#fff",
+            color: "#00395D",
+            fontFamily: "Almarai, sans-serif",
             fontWeight: 400,
-            fontSize: '18px',
-            '&:hover': {
-              backgroundColor: '#f0f0f0',
+            fontSize: "18px",
+            "&:hover": {
+              backgroundColor: "#f0f0f0",
             },
           }}
           onClick={onClose}
         >
           Close
         </Button>
-      </motion.div>
+      </motion.div> */}
     </Box>
   );
 };
--- src/pages/EvaluationPage/EvaluationPage.jsx
+++ src/pages/EvaluationPage/EvaluationPage.jsx
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { Box, Typography, Button, TextField, TextareaAutosize } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  TextareaAutosize,
+} from "@mui/material";
 import starFilled from "../../assets/icons/star-filled.svg";
 import starEmpty from "../../assets/icons/star-empty.svg";
 // import { useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
+import { m, motion } from "framer-motion";
 import { LanguageContext } from "../../context/LanguageContext";
 import AnimatedHeader from "../../components/AnimatedHeader/AnimatedHeader";
 import SuccessPopup from "../../components/SuccessPopup/SuccessPopup";
@@ -20,22 +26,26 @@ const EvaluationPage = () => {
   const [popupOpen, setPopupOpen] = useState(false);
   const [popupType, setPopupType] = useState(""); // 'success' or 'error'
   const [popupMessage, setPopupMessage] = useState("");
-   // Reference for main content area
-   const mainContentRef = useRef(null);
+  // Reference for main content area
+  const mainContentRef = useRef(null);
 
-   // Apply blur effect to the main content when popup is open
-   useEffect(() => {
-     if (popupOpen) {
-       // Apply the blur effect on the main content area
-       if (mainContentRef.current) {
-         mainContentRef.current.style.filter = "blur(5px)";
-       }
-     } else {
-       if (mainContentRef.current) {
-         mainContentRef.current.style.filter = "none";
-       }
-     }
-   }, [popupOpen]);
+  // Apply blur effect to the main content when popup is open
+  useEffect(() => {
+    if (popupOpen) {
+      // Apply blur effect and background overlay when popup is open
+      if (mainContentRef.current) {
+        mainContentRef.current.style.filter = "blur(5px)";
+        mainContentRef.current.style.transition = "filter 0.3s ease-in-out";
+        mainContentRef.current.style.pointerEvents = "none"; // Disable pointer events on main content
+      }
+    } else {
+      // Reset the main content filter and background when the popup is closed
+      if (mainContentRef.current) {
+        mainContentRef.current.style.filter = "none";
+        mainContentRef.current.style.pointerEvents = "auto"; // Enable pointer events on main content
+      }
+    }
+  }, [popupOpen]);
   const ratingLabels = [
     t.Evaluation.ratings.cleanliness,
     t.Evaluation.ratings.service,
@@ -50,18 +60,18 @@ const EvaluationPage = () => {
   };
   const handleSubmit = () => {
     // Example validation: Check if all ratings are provided
-    const isValid = ratings.every(rating => rating !== 0);
-    
+    const isValid = ratings.every((rating) => rating !== 0);
+
     if (isValid) {
       // Show success popup
       setPopupMessage(t.Evaluation.successMessage);
-      setPopupType('success');
+      setPopupType("success");
     } else {
       // Show error popup
       setPopupMessage(t.Evaluation.errorMessage);
-      setPopupType('error');
+      setPopupType("error");
     }
-    
+
     setPopupOpen(true);
   };
   return (
@@ -77,171 +87,173 @@ const EvaluationPage = () => {
         flexDirection: "column",
       }}
     >
-      <Box  ref={mainContentRef}>
-       <AnimatedHeader
-      title={t.Evaluation.header}
-      subtitle={t.Evaluation.subheader}
-    />
+      <Box ref={mainContentRef}>
+        <AnimatedHeader
+          title={t.Evaluation.header}
+          subtitle={t.Evaluation.subheader}
+        />
 
-      {/* Animated Card Container */}
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, ease: "easeOut" }}
-      >
-        <Box
-          sx={{
-            width: { xs: "90%", sm: "440px" }, // Adjust width for small screens
-            // height: { xs: "auto", sm: "400px" },
-            borderRadius: "8px",
-            background: "linear-gradient(180deg, #00395D 0%, #13537C 100%)",
-            p: { xs: 2, sm: 3 },
-            color: "#fff",
-            display: "flex",
-            flexDirection: "column",
-            gap: 3,
-          }}
+        {/* Animated Card Container */}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
         >
-          <Typography
+          <Box
             sx={{
-              fontFamily: "Almarai, sans-serif",
-              fontWeight: 700,
-              fontSize: "20px",
-              // textAlign: "right",
-              // mb: 3,
+              width: { xs: "90%", sm: "440px" }, // Adjust width for small screens
+              // height: { xs: "auto", sm: "400px" },
+              borderRadius: "8px",
+              background: "linear-gradient(180deg, #00395D 0%, #13537C 100%)",
+              p: { xs: 2, sm: 3 },
+              color: "#fff",
+              display: "flex",
+              flexDirection: "column",
+              gap: 3,
             }}
           >
-            {t.Evaluation.rateYourStay}
-          </Typography>
-
-          {ratingLabels.map((label, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: index * 0.15, duration: 0.4 }}
-            >
-              <Box
-                // key={index}
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  flexWrap: "wrap",
-                  gap: 2,
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontFamily: "Almarai, sans-serif",
-                    fontWeight: 400,
-                    fontSize: "18px",
-                    whiteSpace: "normal", // allow line breaks
-                    // textAlign: { xs: "center", sm: "right" }, // center on small screens
-                    width: { xs: "95px", sm: "auto" },
-                  }}
-                >
-                  {label}
-                </Typography>
-                <Box>
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <motion.img
-                      key={star}
-                      src={
-                        star <=
-                        (hovered.index === index
-                          ? hovered.value
-                          : ratings[index])
-                          ? starFilled
-                          : starEmpty
-                      }
-                      alt="star"
-                      style={{
-                        width: 22,
-                        height: 22,
-                        cursor: "pointer",
-                        marginLeft: 4,
-                      }}
-                      whileHover={{ scale: 1.2 }}
-                      transition={{ type: "spring", stiffness: 300 }}
-                      onClick={() => handleRating(index, star)}
-                      onMouseEnter={() => setHovered({ index, value: star })}
-                      onMouseLeave={() => setHovered({ index: null, value: 0 })}
-                    />
-                  ))}
-                </Box>
-              </Box>
-            </motion.div>
-          ))}
-
-          {/* Comment Section */}
-          <Box>
             <Typography
               sx={{
                 fontFamily: "Almarai, sans-serif",
-                fontWeight: 400,
-                fontSize: "18px",
-                mb: 2,
+                fontWeight: 700,
+                fontSize: "20px",
+                // textAlign: "right",
+                // mb: 3,
               }}
             >
-              {t.Evaluation.commentLabel}
+              {t.Evaluation.rateYourStay}
             </Typography>
-            <TextareaAutosize
-              name="comment"
-              minRows={8.5}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder={t.Evaluation.commentPlaceholder}
-               className="styled-placeholder"
-              style={{
-                border: "1px solid #FFFFFF80",
-                borderRadius: "4px",
-                padding: "12px",
-                backgroundColor: "transparent",
-                color: "#fff",
-                width: "90.6%",
-              }}
-            />
-          </Box>
 
-          {/* Submit Button */}
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            transition={{ duration: 0.2 }}
-          >
-            <Button
-              variant="contained"
-              sx={{
-                mt: "6px",
-                width: "100%",
-                height: 48,
-                borderRadius: "5px",
-                backgroundColor: "#00395D",
-                fontFamily: "Almarai, sans-serif",
-                fontWeight: 400,
-                fontSize: "18px",
-                lineHeight: "100%",
-                "&:hover": {
-                  backgroundColor: "#002d4d",
-                },
-              }}
-              onClick={handleSubmit}
+            {ratingLabels.map((label, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: index * 0.15, duration: 0.4 }}
+              >
+                <Box
+                  // key={index}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                    gap: 2,
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "Almarai, sans-serif",
+                      fontWeight: 400,
+                      fontSize: "18px",
+                      whiteSpace: "normal", // allow line breaks
+                      // textAlign: { xs: "center", sm: "right" }, // center on small screens
+                      width: { xs: "95px", sm: "auto" },
+                    }}
+                  >
+                    {label}
+                  </Typography>
+                  <Box>
+                    {[1, 2, 3, 4, 5].map((star) => (
+                      <motion.img
+                        key={star}
+                        src={
+                          star <=
+                          (hovered.index === index
+                            ? hovered.value
+                            : ratings[index])
+                            ? starFilled
+                            : starEmpty
+                        }
+                        alt="star"
+                        style={{
+                          width: 22,
+                          height: 22,
+                          cursor: "pointer",
+                          marginLeft: 4,
+                        }}
+                        whileHover={{ scale: 1.2 }}
+                        transition={{ type: "spring", stiffness: 300 }}
+                        onClick={() => handleRating(index, star)}
+                        onMouseEnter={() => setHovered({ index, value: star })}
+                        onMouseLeave={() =>
+                          setHovered({ index: null, value: 0 })
+                        }
+                      />
+                    ))}
+                  </Box>
+                </Box>
+              </motion.div>
+            ))}
+
+            {/* Comment Section */}
+            <Box>
+              <Typography
+                sx={{
+                  fontFamily: "Almarai, sans-serif",
+                  fontWeight: 400,
+                  fontSize: "18px",
+                  mb: 2,
+                }}
+              >
+                {t.Evaluation.commentLabel}
+              </Typography>
+              <TextareaAutosize
+                name="comment"
+                minRows={8.5}
+                onChange={(e) => setComment(e.target.value)}
+                placeholder={t.Evaluation.commentPlaceholder}
+                className="styled-placeholder"
+                style={{
+                  border: "1px solid #FFFFFF80",
+                  borderRadius: "4px",
+                  padding: "12px",
+                  backgroundColor: "transparent",
+                  color: "#fff",
+                  width: "90.6%",
+                }}
+              />
+            </Box>
+
+            {/* Submit Button */}
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.2 }}
             >
-              {t.Evaluation.submit}
-            </Button>
-          </motion.div>
-        </Box>
-      </motion.div>
+              <Button
+                variant="contained"
+                sx={{
+                  mt: "6px",
+                  width: "100%",
+                  height: 48,
+                  borderRadius: "5px",
+                  backgroundColor: "#00395D",
+                  fontFamily: "Almarai, sans-serif",
+                  fontWeight: 400,
+                  fontSize: "18px",
+                  lineHeight: "100%",
+                  "&:hover": {
+                    backgroundColor: "#002d4d",
+                  },
+                }}
+                onClick={handleSubmit}
+              >
+                {t.Evaluation.submit}
+              </Button>
+            </motion.div>
+          </Box>
+        </motion.div>
       </Box>
-         {/* Success or Error Popups */}
-         {popupType === 'success' && (
+      {/* Success or Error Popups */}
+      {popupType === "success" && (
         <SuccessPopup
           open={popupOpen}
           message={popupMessage}
           onClose={() => setPopupOpen(false)}
         />
       )}
-     {popupType === 'error' && (
+      {popupType === "error" && (
         <ErrorPopup
           open={popupOpen}
           message={popupMessage}
